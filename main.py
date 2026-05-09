852b48b91292c257ea65dcb42ee9e3eb9bc17bf1
Oscar_Rivera <oscarriveramayor@hotmail.com>
2026-05-09 10:54:49 -0500

    post and get endpoints

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from typing import List
-
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException, Depends
 from db import  SessionDep, create_all_tables
-from sqlmodel import select
+from sqlmodel import select, Session, SQLModel, create_engine
+from model import Dog
+from db import engine, get_session
 
 app = FastAPI()
 
+@app.on_event("startup")
+def startup():
+    SQLModel.metadata.create_all(engine)
 
+@app.post("/DOGS", response_model=Dog)
+async def create_dog(dog: Dog, session: Session = Depends(get_session)):
+    session.add(dog)
+    session.commit()
+    session.refresh(dog)
+    return dog
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/DOGS", response_model=List[Dog])
+async def get_all_dogs(session: Session = Depends(get_session)):
+    dogs = session.exec(select(Dog)).all()
+    return dogs
 
 
 @app.get("/hello/{name}")
