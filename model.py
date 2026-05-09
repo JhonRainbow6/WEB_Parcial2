f74f26c19e44d5fb84a6e8bd6536c9bdc0c395fa
Oscar_Rivera <oscarriveramayor@hotmail.com>
2026-05-08 20:50:41 -0500

    model create

--- model.py
+++ model.py
@@ -1,11 +1,16 @@
 from sqlmodel import Field, SQLModel
-
+from typing import Optional
 from datetime import datetime, timezone
 
 
 class Dog(SQLModel, table = True):
     __tablename__ = "dogs"
-
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    size: str
+    dangerous: bool
+    sterilized: bool
+    breed: str
     created: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 class Sticker(SQLModel, table = True):
