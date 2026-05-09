e83049864650a440273739e705dd0d5795e157e6
Oscar_Rivera <oscarriveramayor@hotmail.com>
2026-05-09 11:10:01 -0500

    patch and delete endpoints

--- main.py
+++ main.py
@@ -23,6 +23,29 @@ async def get_all_dogs(session: Session = Depends(get_session)):
     dogs = session.exec(select(Dog)).all()
     return dogs
 
+@app.patch("/Dogs/{dog_id}", response_model=Dog)
+async def update_dog(dog_id: int, dog_data: dict, session: Session = Depends(get_session)):
+    dogs = session.get(Dog, dog_id)
+    if not dogs:
+        raise HTTPException(status_code=404, detail="dog not found")
+
+    for key, value in dog_data.items():
+        if hasattr(dogs, key):
+            setattr(dogs, key, value)
+
+    session.add(dogs)
+    session.commit()
+    session.refresh(dogs)
+    return dogs
+
+@app.delete("/Dogs/{dog_id}", response_model=Dog)
+async def delete_dog(dog_id: int, session: Session = Depends(get_session)):
+    dogs = session.get(Dog, dog_id)
+    if not dogs:
+        raise HTTPException(status_code=404, detail="dog not found")
+    session.delete(dogs)
+    session.commit()
+    return dogs
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
